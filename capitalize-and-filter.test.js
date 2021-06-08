be721d98169f276d382d095e454190ee310d000f
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-06-08 11:02:01 -0700

    changed test capitalize and filter

--- capitalize-and-filter.test.js
+++ capitalize-and-filter.test.js
@@ -2,7 +2,7 @@ import capitalizeAndFilter from './capitalize-and-filter.js';
 
 describe('capitalizeAndFilter', () => {
   it('takes an array of strings capitalize all strings and filter out any string that starts with the letter F/f', () => {
-    const stringArray = ['favorite', 'clear', 'monkey', 'Frank', false, []];
-    expect(capitalizeAndFilter(stringArray)).toEqual(['CLEAR', 'MONKEY', false, []]);
+    const stringArray = ['favorite', 'dog', 'monkey', 'Frakie', 'cat'];
+    expect(capitalizeAndFilter(stringArray)).toEqual(['DOG', 'MONKEY', 'CAT']);
   });
 });
